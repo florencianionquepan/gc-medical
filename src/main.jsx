12bce808d4d5da7661f1daa44cb3a2db9e893b34
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-04-25 22:10:09 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { MIHeader } from './assets/MIHeader.jsx'
+import { App } from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MIHeader/>
+    <App/>
   </React.StrictMode>,
 )
