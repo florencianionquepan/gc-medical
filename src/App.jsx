d618a3618c7d2b31f7d9dadb33a01395658ef7b6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-04-25 22:10:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { MIHeader } from './assets/MIHeader'
+import { Hero } from './home/Hero'
 
 export const App = () => {
   return (
+    <>
     <MIHeader/>
+    <Hero/>
+    </>
   )
 }
 
