ba31d551f0f17c24701def4b9f3151723b73d8eb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 19:59:55 -0300

    Update MiHeader.jsx

--- src/assets/MiHeader.jsx
+++ src/assets/MiHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { MiNavbar } from './MiNavbar';
 
 
 
@@ -17,7 +17,7 @@ export const MiHeader = () => {
         </form>
       </div>
     </header>
-    
+    <MiNavbar/>
   </div>
   )
 }
