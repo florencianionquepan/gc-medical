9b7936bcb723ccd5357f56cce63c6f97f1f7e02d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 19:59:23 -0300

    configure main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-import { MiHeader } from './assets/MiHeader'
-import { Hero } from './home/Hero'
+import { MiHeader } from './assets/MiHeader';
+import {HomePage} from './home/HomePage';
+import {ContactPage} from './contact/ContactPage';
+import {AboutPage} from './about/AboutPage';
+import { Navigate, Route, Routes } from "react-router-dom";
 
 export const App = () => {
   return (
     <>
     <MiHeader/>
-    <Hero/>
+      <Routes>
+        <Route path="/" element={ <HomePage/> }/>
+        <Route path="nosotros" element={ <AboutPage/> }/>
+        <Route path="contacto" element={ <ContactPage/> }/>
+        <Route path="/*" element={ <Navigate to="/"/> }/>                                                
+      </Routes>
     </>
   )
 }
