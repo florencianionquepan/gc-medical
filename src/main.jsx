0727bd2bef1ee6029f4e87bfd39d0de2da9adabd
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 19:59:59 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { MIHeader } from './assets/MIHeader.jsx'
 import { App } from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </BrowserRouter>
 )
