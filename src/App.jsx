910516bffc9ef5cf9eae8669b5ff2431b3188532
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 19:39:26 -0300

    change name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { MIHeader } from './assets/MIHeader'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+import { MiHeader } from './assets/MiHeader'
 import { Hero } from './home/Hero'
 
 export const App = () => {
   return (
     <>
-    <MIHeader/>
+    <MiHeader/>
     <Hero/>
     </>
   )
---
+++ src/assets/MiHeader.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+
+
+export const MiHeader = () => {
+
+
+  return (
+  <div className='sticky-top'>
+    <header className="py-3 border-bottom">
+      <div className="d-flex flex-wrap justify-content-center">
+        <a href="/" className="d-flex align-items-center ms-4 mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+          <img src="img/logo.png" alt="logo"/>
+        </a>
+        <form className="col-10 col-lg-auto mb-3 mb-lg-0 mx-5 me-lg-4 align-self-center" role="search">
+          <input type="search" className="form-control" placeholder="Buscar..." aria-label="Search"/>
+        </form>
+      </div>
+    </header>
+    
+  </div>
+  )
+}
