d5549f1c260a5608107cfef30743a458788f45a1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 19:39:04 -0300

    Create Hero.jsx

---
+++ src/home/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div style={{ 
+        backgroundImage: `url("img/background.png")`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        minHeight: '600px',
+        }}>
+        {/* <div className="row">
+          <div className="col-lg-6 mt-5">
+            <h1 className="">Our Path to Success</h1>
+            <p className="hero-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#" className="btn btn-primary">Learn More</a>
+          </div>
+        </div> */}
+    </div>
+  )
+}
