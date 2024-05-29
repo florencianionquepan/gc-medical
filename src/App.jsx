dbd72841ade9bdd930609d7039522acbd04c8166
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-29 20:39:37 -0300

    add imports to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-
 import {HomePage} from './home/HomePage';
 import {ContactPage} from './contact/ContactPage';
 import {AboutPage} from './about/AboutPage';
