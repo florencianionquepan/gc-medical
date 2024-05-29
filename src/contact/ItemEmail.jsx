332eb2a9edb10ec3596339d6c54664944914d6d5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-29 20:52:21 -0300

    Create ItemEmail.jsx

---
+++ src/contact/ItemEmail.jsx
@@ -0,0 +1,22 @@
+import { BigIcon } from "./BigIcon"
+
+export const ItemEmail = () => {
+  return (
+    <>
+        <div className="border rounded border-secondary border-3 position-relative pt-5">
+            <BigIcon iconClass={"bi bi-envelope-arrow-up"}/>
+            <p className="px-4 pt-3 mt-5">
+                info@gcmedical.com.ar
+            </p>
+            <p className="px-4">
+            Podés encontrarnos en otras redes sociales 
+                    Instagram:
+                    @GCMedicalbb
+                    @GcMedicaldiabetes
+                    Linkedin
+                    Facebook
+            </p>
+        </div>
+    </>
+  )
+}
