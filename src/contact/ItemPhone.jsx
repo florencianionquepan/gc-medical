58d4edbe176f5dfa304608503d6a712a0098fd06
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-29 20:48:15 -0300

    Create ItemPhone.jsx

---
+++ src/contact/ItemPhone.jsx
@@ -0,0 +1,17 @@
+import { BigIcon } from "./BigIcon"
+
+export const ItemPhone = () => {
+  return (
+    <>
+        <div className="border rounded border-secondary border-3 position-relative pt-5">
+            <BigIcon iconClass={"bi bi-phone-vibrate"}/>
+            <p className="px-4 pt-3 mt-5">
+                +54 291 37-2900
+            </p>
+            <p className="px-4">
+                En nuestro Whatsapp tambien podes encontrar nuestro catalogo!
+            </p>
+        </div>
+    </>
+  )
+}
