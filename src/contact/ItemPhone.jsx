4dacc843e3418f657ee7e9d581c91bc55489d1f6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-29 21:30:20 -0300

    Update ItemPhone.jsx

--- src/contact/ItemPhone.jsx
+++ src/contact/ItemPhone.jsx
@@ -3,7 +3,7 @@ import { BigIcon } from "./BigIcon"
 export const ItemPhone = () => {
   return (
     <>
-        <div className="border rounded border-secondary border-3 position-relative pt-5">
+        <div className="border rounded border-secondary border-3 position-relative pt-5 my-0 my-md-5">
             <BigIcon iconClass={"bi bi-phone-vibrate"}/>
             <p className="px-4 pt-3 mt-5">
                 +54 291 37-2900
