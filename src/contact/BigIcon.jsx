275242526d2b5bf5664ba42c73a68ef5da892518
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-29 21:34:42 -0300

    ajuste de estilos

--- src/contact/BigIcon.jsx
+++ src/contact/BigIcon.jsx
@@ -4,8 +4,8 @@ export const BigIcon = ({iconClass}) => {
         <div className="border border-secondary border-3 rounded-circle w-50 mx-auto
         d-flex justify-content-center align-items-center
         position-absolute top-0 start-50 translate-middle bg-body" 
-        style={{ aspectRatio: '1 / 1'}}>
-        <i className={iconClass} style={{ fontSize: '5rem'}}></i>
+        style={{ aspectRatio: '1 / 1', maxWidth: '170px', '@media (min-width: 1250px)': ''}}>
+        <i className={iconClass} style={{ fontSize: '400%'}}></i>
         </div>
     </>
   )
--- src/contact/ContactPage.jsx
+++ src/contact/ContactPage.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
+import { ItemLocation } from './ItemLocation'
+import { ItemPhone } from './ItemPhone'
+import { ItemEmail } from './ItemEmail'
 
 export const ContactPage = () => {
   return (
     <>
       <div className='container-md my-5 text-center mx-auto'>
-        <div className='row justify-content-center'>
+        <div className='row justify-content-center mt-5'>
           {/* Primera columna */}
-          <div className="col-lg-4 m-1">
-            <div className="row justify-content-center h-100">
-              <div className="col-10 h-100 border border-secondary">
-                    PRIMERA COLUMNA
+          <div className="col-12 col-sm-10 col-lg-6 col-xxl-5 mt-5">
+            <div className="row justify-content-center h-100 mt-5 mb-1 mb-md-5">
+              <div className="col-10 h-100">
+                    <ItemLocation/>
               </div>
             </div>
           </div>
 
           {/* Segunda columna */}
-          <div className="col-lg-4 mx-1">
-            <div className="row justify-content-center">
-              <div className="col-10 border border-secondary my-1">
-                    TELEFONO
+          <div className="col-12 col-sm-10 col-lg-6 col-xxl-5">
+            <div className="row justify-content-center my-5 my-md-1">
+              <div className="col-10 my-5">
+                    <ItemPhone/>
               </div>
 
-              <div className="col-10 border border-secondary my-1">
-                    EMAIL
+              <div className="col-10 my-5">
+                    <ItemEmail/>
               </div>
 
             </div>
