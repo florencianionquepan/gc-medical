155e80ab7492818e4542cbfa87cdf6adee628761
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-12 20:10:33 -0300

    add data of some products

---
+++ src/products/data/nefrologia.js
@@ -0,0 +1,38 @@
+export const urologia=[
+    {
+        id:91,
+        name:'Kit de Nefrostomia',
+        provider:'Promedon',
+        image:'nefrosotomia1.webp',
+        description:"El kit de nefrostomía incluye agujas con puntas de dos tipos: chiba y trocar, catéter de drenaje mono jota, tres dilatadores fasciales, una guía de PTFE, estilete y conector a bolsa de orina"
+    },
+    {
+        id:92,
+        name:'Cateter Ureteral',
+        provider:'Promedon',
+        image:'nefrosotomia2.webp',
+        description:"El Catéter Ureteral es un elemento con múltiples indicaciones en procedimientos endourológicos"
+    },
+    {
+        id:93,
+        name:'Indrive Vaina de acceso ureteral',
+        provider:'Promedon',
+        image:'nefrosotomia3.webp',
+        description:"Permite el paso simple del instrumental durante una ureteroscopía flexible para la extracción de cálculos renales.Reduce las probabilidades de traumatismo en el uréter durante el intercambio repetido de instrumentos. Crea un canal de trabajo continuo que protege los instrumentos delicados y los endoscopios flexibles de daños, lo que puede reducir la necesidad de reparaciones costosas. Permite mejorar el flujo de irrigación con el canal de trabajo continuo sin tortuosidad ni bloqueos por lo que ayuda a reducir la presión intra-renal."
+    },
+    {
+        id:94,
+        name:'Set De Dilatación Renal PERKUT',
+        provider:'Promedon',
+        image:'nefrosotomia3.webp',
+        description:"Permite el paso simple del instrumental durante una ureteroscopía flexible para la extracción de cálculos renales.Reduce las probabilidades de traumatismo en el uréter durante el intercambio repetido de instrumentos. Crea un canal de trabajo continuo que protege los instrumentos delicados y los endoscopios flexibles de daños, lo que puede reducir la necesidad de reparaciones costosas. Permite mejorar el flujo de irrigación con el canal de trabajo continuo sin tortuosidad ni bloqueos por lo que ayuda a reducir la presión intra-renal."
+    },
+    {
+        id:94,
+        name:'Set De Dilatación Renal PERKUT',
+        provider:'Promedon',
+        image:'nefrosotomia4.webp',
+        description:"El material del catéter guía y las vainas de dilatación es PTFE y el de los dilatadores, polipropileno.El set se compone de un catéter guía de 8 Fr, una vaina introductoria de 10 Fr, 4 vainas (24-26-28-30 Fr) y 13 dilatadores de 8 a 30 Fr. La longitud de los dilatadores es de 23 a 30 cm. La longitud de las vainas es de 17cm. Longitud del catéter: 80cm."
+    }
+    
+];
---
+++ src/products/data/urologia.js
@@ -0,0 +1,24 @@
+export const urologia=[
+    {
+        id:81,
+        name:'Steema Dual TOT',
+        provider:'Promedon',
+        image:'urologia1.webp',
+        description:"STEEMA DUAL TOT es un sistema de cabestrillo transobturador, desarrollado para el tratamiento de la incontinencia urinaria femenina. El sistema de cabestrillo transvaginal, equipado con dilatadores en cada lado, la vaina de Teflon® y la pestaña de posicionamiento, ofrece todas las características que ayudan al cirujano a realizar una implantación transobturadora fácil, segura y suave desde la dirección que elija. Una vez finalizada la implantación, sólo se deja en el paciente el cabestrillo fabricado en polipropileno tipo 1 para dar soporte a la uretra y por tanto restablecer la continencia. Inmediatamente después de la implantación, antes de que el tejido crezca hacia el interior, el cabestrillo se ancla al tejido mediante un efecto tipo Velcro® y funciona sin fijación adicional."
+    },
+    {
+        id:82,
+        name:'Sistema De Reparación De Prolapso Apical SPLENTIS®',
+        provider:'Promedon',
+        image:'urologia2.webp',
+        description:"SPLENTIS es un sistema de reparación de una sola incisión, indicado para la cervicopexia sacroespinosa bilateral transvaginal del prolapso apical del órgano pélvico en mujeres no fértiles. El implante tipo cabestrillo se fija bilateralmente a los ligamentos sacroespinosos y centralmente a la porción supravaginal anterior del cuello uterino. Esta configuración de implante utiliza la cantidad mínima de material aloplástico, no interfiere con la pared vaginal anterior y, por lo tanto, demuestra un excelente éxito del tratamiento al tiempo que minimiza los eventos adversos."
+    },
+    {
+        id:83,
+        name:'Sistema De Sling Suburetheral UNITAPE VS',
+        provider:'Promedon',
+        image:'urologia3.png',
+        description:"Lanzado en 2006, Unitape sirvió para mejorar la calidad de vida de las mujeres que sufren de incontinencia urinaria de esfuerzo, en más de 100.000 cirugías en todo el mundo. Unitape muestra el mismo rendimiento que otros cabestrillos de uretra media bien establecidos, que están o han estado en el mercado1,2,3,4,5,6,7. La experiencia de la cirugía TOT con Unitape T Plus ha mostrado excelentes resultados para la resolución de la IUE femenina con baja tasa de complicaciones y alta tasa de satisfacción a largo plazo."
+    },
+    
+];
