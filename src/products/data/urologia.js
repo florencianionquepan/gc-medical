51e314f7d54deb31f553d6c199a0d6aa7b576a76
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-12 21:32:50 -0300

    Update urologia.js

--- src/products/data/urologia.js
+++ src/products/data/urologia.js
@@ -3,14 +3,14 @@ export const urologia=[
         id:81,
         name:'Steema Dual TOT',
         provider:'Promedon',
-        image:'urologia1.webp',
+        image:'urologia1.png',
         description:"STEEMA DUAL TOT es un sistema de cabestrillo transobturador, desarrollado para el tratamiento de la incontinencia urinaria femenina. El sistema de cabestrillo transvaginal, equipado con dilatadores en cada lado, la vaina de Teflon® y la pestaña de posicionamiento, ofrece todas las características que ayudan al cirujano a realizar una implantación transobturadora fácil, segura y suave desde la dirección que elija. Una vez finalizada la implantación, sólo se deja en el paciente el cabestrillo fabricado en polipropileno tipo 1 para dar soporte a la uretra y por tanto restablecer la continencia. Inmediatamente después de la implantación, antes de que el tejido crezca hacia el interior, el cabestrillo se ancla al tejido mediante un efecto tipo Velcro® y funciona sin fijación adicional."
     },
     {
         id:82,
         name:'Sistema De Reparación De Prolapso Apical SPLENTIS®',
         provider:'Promedon',
-        image:'urologia2.webp',
+        image:'urologia2.png',
         description:"SPLENTIS es un sistema de reparación de una sola incisión, indicado para la cervicopexia sacroespinosa bilateral transvaginal del prolapso apical del órgano pélvico en mujeres no fértiles. El implante tipo cabestrillo se fija bilateralmente a los ligamentos sacroespinosos y centralmente a la porción supravaginal anterior del cuello uterino. Esta configuración de implante utiliza la cantidad mínima de material aloplástico, no interfiere con la pared vaginal anterior y, por lo tanto, demuestra un excelente éxito del tratamiento al tiempo que minimiza los eventos adversos."
     },
     {
