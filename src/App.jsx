d7f329743070570a117e4c35c9cf948b35a6a4ed
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-12 22:50:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import {HomePage} from './home/HomePage';
-import {ContactPage} from './contact/ContactPage';
-import {AboutPage} from './about/AboutPage';
-import { Navigate, Route, Routes } from "react-router-dom";
+import { AppRouter } from "./routes/AppRouter";
 import { MiNavbar } from "./assets/MiNavbar";
 import { MiFooter } from './assets/MiFooter';
+
 
 export const App = () => {
   return (
     <>
     <MiNavbar/>
-      <Routes>
-        <Route path="/" element={ <HomePage/> }/>
-        <Route path="nosotros" element={ <AboutPage/> }/>
-        <Route path="contacto" element={ <ContactPage/> }/>
-        <Route path="/*" element={ <Navigate to="/"/> }/>                                                
-      </Routes>
+      <AppRouter/>
     <MiFooter/>
     </>
   )
