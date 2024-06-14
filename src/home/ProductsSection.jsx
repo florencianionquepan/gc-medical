c8a04859bed2cd804c1eb996c555ffa809179b73
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-14 20:04:25 -0300

    Create ProductsSection.jsx

---
+++ src/home/ProductsSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const ProductsSection = () => {
+  return (
+    <>
+        <div className='m-5 p-5 border border-secondary rounded'>
+            <h2> Nuestros Productos </h2>
+            <div className='row'>
+                <div className='col-12'>
+                Poner algunas cards con algunos de los productos podria ser, o si arriba dejamos ese carousel ir poniendo las lineas
+                que se manejan, poner algo de animacion
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
