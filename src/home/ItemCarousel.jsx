3088b184ee3e1266e5440516e84a15dd3b439178
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-14 20:49:19 -0300

    fixes responsive

--- src/home/ItemCarousel.jsx
+++ src/home/ItemCarousel.jsx
@@ -2,15 +2,15 @@
 
 export const ItemCarousel = ({isActive, imagePath, image, positionText, title, description1, description2}) => {
 
-const dynamicClass=`position-absolute ${positionText} p-2 p-xl-4 p-xxl-5 rounded mt-5 ms-3 ms-lg-5 bg-white bg-opacity-75`
+const dynamicClass=`position-absolute ${positionText} rounded bg-white bg-opacity-75 item-sm`
 
   return (
     <div className={`carousel-item ${isActive ? 'active' : ''} position-relative`}>
         <img src={imagePath} className="d-block w-100" alt={image}/>
         <div className={dynamicClass}>
-            <h2 className="text-resp">{title}</h2>
-            <p className="text-resp mb-0">{description1}</p>
-            <p className="text-resp"> {description2} </p>      
+            <h2 className="title-resp text-dark">{title}</h2>
+            <p className="text-resp mb-0 text-dark">{description1}</p>
+            <p className="text-resp mb-0 text-dark"> {description2} </p>      
         </div>    
     </div>
   )
