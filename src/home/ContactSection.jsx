40d759aad2a03097798fed8e16f6ea31ec6464ee
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-14 20:04:27 -0300

    Create ContactSection.jsx

---
+++ src/home/ContactSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const ContactSection = () => {
+  return (
+    <>
+        <div className='p-5 m-5 border border-secondary rounded'>
+            <h2> Seccion contacto </h2>
+            <div className='row'>
+                <div className='col-12'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sed delectus accusamus sit laudantium debitis nostrum. Fuga pariatur molestiae sit enim laudantium commodi labore quaerat cupiditate minus tempora, quo nisi.
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
