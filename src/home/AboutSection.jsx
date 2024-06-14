135b8b127a6374e30f1cd8a97f9cb2505135ee8f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-14 20:04:32 -0300

    Create AboutSection.jsx

---
+++ src/home/AboutSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const AboutSection = () => {
+  return (
+    <div className='m-5 p-5 border border-secondary rounded'>
+            <h2> Sobre nosotros...</h2>
+            <div className='row'>
+                <div className='col-12'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga enim illum sed temporibus, rerum hic repellendus molestias ratione quod ipsam praesentium eos sapiente fugiat eligendi autem veniam officia perspiciatis accusantium.
+                </div>
+            </div>
+        </div>
+  )
+}
