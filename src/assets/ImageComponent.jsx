b51e2b797a532468e5b7706c21ca8830e5716398
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-16 19:13:20 -0300

    Create ImageComponent.jsx

---
+++ src/assets/ImageComponent.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import {BlurhashCanvas} from "react-blurhash";
+import "./imageComponent.css";
+
+export const ImageComponent = ({hash, claseHash, path, clase, imageAlt}) => {
+
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [hideBlurhash, setHideBlurhash] = useState(false);
+
+  useEffect(() => {
+    const img= new Image()
+    img.onload = () =>{
+      setImageLoaded(true);
+      setTimeout(() => setHideBlurhash(true), 800); 
+    }
+    img.src= path
+  }, [path])
+
+
+  return (
+    <>
+      {!hideBlurhash &&
+        <div className={`${imageLoaded? 'no-blurhash':'img-blur'}`} >
+          <BlurhashCanvas
+              hash={hash}
+              punch={1} 
+              className={claseHash}/>
+        </div>}
+        { imageLoaded && (
+            <img
+            src={path}
+            alt={imageAlt}
+            className={clase}
+        />)}
+    </>
+  )
+}
