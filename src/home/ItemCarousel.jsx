d375b4e130d30fe4d7b8166fa637a07d0da50e9c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-16 19:13:45 -0300

    Update ItemCarousel.jsx

--- src/home/ItemCarousel.jsx
+++ src/home/ItemCarousel.jsx
@@ -1,12 +1,16 @@
+import {ImageComponent} from "../assets/ImageComponent";
 
-
-export const ItemCarousel = ({isActive, imagePath, image, positionText, title, description1, description2}) => {
+export const ItemCarousel = ({isActive, imagePath, imageAlt, hash, positionText, title, description1, description2}) => {
 
 const dynamicClass=`position-absolute ${positionText} rounded bg-white bg-opacity-75 item-sm`
 
   return (
     <div className={`carousel-item ${isActive ? 'active' : ''} position-relative`}>
-        <img src={imagePath} className="d-block w-100" alt={image}/>
+      <ImageComponent hash={hash}
+                      claseHash={"img-hash"}
+                      path={imagePath} 
+                      clase={"d-block w-100"}
+                      imageAlt={imageAlt}/>
         <div className={dynamicClass}>
             <h2 className="title-resp text-dark">{title}</h2>
             <p className="text-resp mb-0 text-dark">{description1}</p>
