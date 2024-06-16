6f02d146d90c3a8d8e12a247e3ca626568298419
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-16 20:07:48 -0300

    add images of carousel

--- src/products/data/2_oncologia.js
+++ src/products/data/2_oncologia.js
@@ -1,24 +1,18 @@
-export const heridas=[
+export const oncologia=[
     {
-        id:71,
-        name:'Bomba Genadyne',
-        provider:'Promedon',
-        image:'heridas1.png',
-        description:"Equipo de presión negativa XLR8+ es una bomba potente que maneja fácilmente heridas durante períodos prolongados, ofreciendo opciones de ajuste de presión continua y variable, así como opciones de ajuste de nivel de intensidad. Aprobado por la FDA para atención hospitalaria y domiciliaria.La avanzada capacidad de presión variable del XLR8 + permite utilizar diferentes presiones negativas sin los inconvenientes de la terapia intermitente. Al mantener la presión negativa durante el ciclo, la TPN variable evita la pérdida del sello hermético y el desplazamiento del apósito. Esta «onda» gradual es más suave para el paciente, previniendo la isquemia."
-    },
-    {
-        id:72,
+        id:21,
         name:'Aguja Poly Port Safe',
         provider:'Lexel SRL',
-        image:'heridas2.jpg',
+        image:'oncologia1.jpg',
+        hash:'',
         description:"Aguja Poly Port Safe (Poly Port SF), con tubo de acero de pared ultra delgada y punta tipo Huber, posee un cabezal que permite la segura inserción en el Portal y la fijación al cuerpo del paciente para infusiones prolongadas."
     },
     {
-        id:73,
+        id:22,
         name:'Port Implantable de Titanio',
         provider:'Lexel SRL',
-        image:'heridas3.jpg',
+        image:'oncologia3.jpg',
+        hash:'',
         description:"Es un sistema implantable para la administración de fármacos construido en titanio. Esto hace a este portal extra-liviano, de máxima resistencia y absoluta biocompatibilidad. Su septum autosellante de silicona, permite mas de 1000 punciones con agujas de punta tipo Huber (G21) sin que se produzcan pérdidas. Existen tres diferentes tamaños: Adulto, Pediatrico y el exclusivo modelo Neonatal. El cateter construido en caucho de silicona es radiopaco especialmente diseñado para uso venoso (9, 7 y 5,5 Fr) y peritoneal."
     },
-    
-];
+]
--- src/products/data/3_urologia.js
+++ src/products/data/3_urologia.js
@@ -1,24 +1,57 @@
 export const urologia=[
     {
-        id:81,
+        id:31,
         name:'Steema Dual TOT',
         provider:'Promedon',
         image:'urologia1.png',
+        hash:'',
         description:"STEEMA DUAL TOT es un sistema de cabestrillo transobturador, desarrollado para el tratamiento de la incontinencia urinaria femenina. El sistema de cabestrillo transvaginal, equipado con dilatadores en cada lado, la vaina de Teflon® y la pestaña de posicionamiento, ofrece todas las características que ayudan al cirujano a realizar una implantación transobturadora fácil, segura y suave desde la dirección que elija. Una vez finalizada la implantación, sólo se deja en el paciente el cabestrillo fabricado en polipropileno tipo 1 para dar soporte a la uretra y por tanto restablecer la continencia. Inmediatamente después de la implantación, antes de que el tejido crezca hacia el interior, el cabestrillo se ancla al tejido mediante un efecto tipo Velcro® y funciona sin fijación adicional."
     },
     {
-        id:82,
+        id:32,
         name:'Sistema De Reparación De Prolapso Apical SPLENTIS®',
         provider:'Promedon',
         image:'urologia2.png',
+        hash:'',
         description:"SPLENTIS es un sistema de reparación de una sola incisión, indicado para la cervicopexia sacroespinosa bilateral transvaginal del prolapso apical del órgano pélvico en mujeres no fértiles. El implante tipo cabestrillo se fija bilateralmente a los ligamentos sacroespinosos y centralmente a la porción supravaginal anterior del cuello uterino. Esta configuración de implante utiliza la cantidad mínima de material aloplástico, no interfiere con la pared vaginal anterior y, por lo tanto, demuestra un excelente éxito del tratamiento al tiempo que minimiza los eventos adversos."
     },
     {
-        id:83,
+        id:33,
         name:'Sistema De Sling Suburetheral UNITAPE VS',
         provider:'Promedon',
         image:'urologia3.png',
+        hash:'',
         description:"Lanzado en 2006, Unitape sirvió para mejorar la calidad de vida de las mujeres que sufren de incontinencia urinaria de esfuerzo, en más de 100.000 cirugías en todo el mundo. Unitape muestra el mismo rendimiento que otros cabestrillos de uretra media bien establecidos, que están o han estado en el mercado1,2,3,4,5,6,7. La experiencia de la cirugía TOT con Unitape T Plus ha mostrado excelentes resultados para la resolución de la IUE femenina con baja tasa de complicaciones y alta tasa de satisfacción a largo plazo."
     },
+    {
+        id:34,
+        name:'Kit de Nefrologia',
+        provider:'Promedon',
+        image:'urologia4.png',
+        hash:'',
+        description:"El kit de nefrostomía incluye agujas con puntas de dos tipos: chiba y trocar, catéter de drenaje mono jota, tres dilatadores fasciales, una guía de PTFE, estilete y conector a bolsa de orina"
+    },
+    {
+        id:35,
+        name:'Cateter Ureteral',
+        provider:'Promedon',
+        image:'urologia5.png',
+        hash:'',
+        description:"El Catéter Ureteral es un elemento con múltiples indicaciones en procedimientos endourológicos"
+    },
+    {
+        id:36,
+        name:'Indrive Vaina de acceso ureteral',
+        provider:'Promedon',
+        image:'urologia6.png',
+        description:"Permite el paso simple del instrumental durante una ureteroscopía flexible para la extracción de cálculos renales.Reduce las probabilidades de traumatismo en el uréter durante el intercambio repetido de instrumentos. Crea un canal de trabajo continuo que protege los instrumentos delicados y los endoscopios flexibles de daños, lo que puede reducir la necesidad de reparaciones costosas. Permite mejorar el flujo de irrigación con el canal de trabajo continuo sin tortuosidad ni bloqueos por lo que ayuda a reducir la presión intra-renal."
+    },
+    {
+        id:37,
+        name:'Set De Dilatación Renal PERKUT',
+        provider:'Promedon',
+        image:'urologia7.png',
+        description:"Permite el paso simple del instrumental durante una ureteroscopía flexible para la extracción de cálculos renales.Reduce las probabilidades de traumatismo en el uréter durante el intercambio repetido de instrumentos. Crea un canal de trabajo continuo que protege los instrumentos delicados y los endoscopios flexibles de daños, lo que puede reducir la necesidad de reparaciones costosas. Permite mejorar el flujo de irrigación con el canal de trabajo continuo sin tortuosidad ni bloqueos por lo que ayuda a reducir la presión intra-renal."
+    }
     
 ];
