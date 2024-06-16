291a02f703cc57a5b9644f63f6d59db2c8df0759
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-16 20:39:51 -0300

    Update ProductLinePage.jsx

--- src/products/ProductLinePage.jsx
+++ src/products/ProductLinePage.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
+import { getProductsByLine } from './helpers/getProductsByLine';
+import {ProductCard} from './ProductCard';
 
 export const ProductLinePage = () => {
 
   const {linea} = useParams();
   const navigate = useNavigate();
 
+  const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      const products = getProductsByLine(linea);
+      setProducts(products);
+    }, []);
+
   return (
     <div className='container-md'>
-      <h5>
-      ProductLinePage : {linea} 
-      </h5>
+        <div className='row m-5'>
+            <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" className="form-control" placeholder="Buscar producto..." aria-label="Search"/>
+            </form>
+        </div>
+        <div className='row m-5'>
+            { products.map((pro)=>(
+                <div key={pro.id} className="col-6 col-md-4 mb-3">
+                    <ProductCard product={pro}/>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
