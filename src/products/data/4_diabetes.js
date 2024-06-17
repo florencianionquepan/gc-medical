f41c30e6f47f939220fa20fabc397548033ad415
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-16 21:21:47 -0300

    Update 4_diabetes.js

--- src/products/data/4_diabetes.js
+++ src/products/data/4_diabetes.js
@@ -1,3 +1,59 @@
 export const diabetes=[
-    
+    {
+        id:41,
+        name:'Sistema MiniMed 780G',
+        provider:'Medtronic',
+        image:'diabetes1.jpg',
+        hash:'',
+        description:''
+    },
+    {
+        id:42,
+        name:'Sistema MiniMed 670G',
+        provider:'Medtronic',
+        image:'diabetes2.jpg',
+        hash:'',
+        description:''
+    },
+    {
+        id:43,
+        name:'Guardian Sensor Transmisor',
+        provider:'Medtronic',
+        image:'diabetes3.jpg',
+        hash:'',
+        description:''
+    },
+    {
+        id:44,
+        name:'I Port Advance',
+        provider:'Medtronic',
+        image:'diabetes4.jpg',
+        hash:'',
+        description:''
+    },
+    {
+        id:45,
+        name:'Reservorio MiniMed',
+        provider:'Medtronic',
+        image:'diabetes5.jpg',
+        hash:'',
+        description:''
+    },
+    {
+        id:46,
+        name:'Quick Set',
+        provider:'Medtronic',
+        image:'diabetes6.jpg',
+        hash:'',
+        description:''
+    },
+    {
+        id:47,
+        name:'Set de infusión Silhouette',
+        provider:'Medtronic',
+        image:'diabetes7.jpg',
+        hash:'',
+        description:''
+    },
+
 ];
