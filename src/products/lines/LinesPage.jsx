db3c16c66d735dbe178a069030685f5ccae9843b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-17 17:46:31 -0300

    Create LinesPage.jsx

---
+++ src/products/lines/LinesPage.jsx
@@ -0,0 +1,10 @@
+import { LineComponent } from "./LineComponent"
+
+
+export const LinesPage = () => {
+  return (
+    <div className="container-md">
+        <LineComponent linea={"Tratamiento de heridas"}/>
+    </div>
+  )
+}
