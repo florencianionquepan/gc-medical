55120e735755fe2f54ae61ee1b03436dab4857de
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-16 21:21:32 -0300

    Update 1_heridas.js

--- src/products/data/1_heridas.js
+++ src/products/data/1_heridas.js
@@ -3,7 +3,7 @@ export const heridas=[
         id:11,
         name:'Bomba Genadyne',
         provider:'Promedon',
-        image:'heridas1.png',
+        image:'heridas1.jpg',
         hash:'',
         description:"Equipo de presión negativa XLR8+ es una bomba potente que maneja fácilmente heridas durante períodos prolongados, ofreciendo opciones de ajuste de presión continua y variable, así como opciones de ajuste de nivel de intensidad. Aprobado por la FDA para atención hospitalaria y domiciliaria.La avanzada capacidad de presión variable del XLR8 + permite utilizar diferentes presiones negativas sin los inconvenientes de la terapia intermitente. Al mantener la presión negativa durante el ciclo, la TPN variable evita la pérdida del sello hermético y el desplazamiento del apósito. Esta «onda» gradual es más suave para el paciente, previniendo la isquemia."
     }
