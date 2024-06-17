fdc2dc89dcca8ad48345b79c38c31f1df1b8336d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-17 17:00:07 -0300

    add the other lines to all

--- src/products/data/all.js
+++ src/products/data/all.js
@@ -1,19 +1,34 @@
-import { heridas } from "./heridas";
-import { nefrologia } from "./nefrologia";
-import { urologia } from "./urologia";
+import { heridas } from "./1_heridas";
+import { oncologia } from "./2_oncologia";
+import { urologia } from "./3_urologia";
+import { diabetes } from "./4_diabetes";
+import { neurologia } from './../data/5_neurologia';
+import { cardiologia } from './../data/6_cardiologia';
+import { gastroenterologia } from './../data/7_gastroenterologia';
+import { cirugia } from './../data/8_cirugia';
 
 const addLine = (products, nameLine) => {
     return products.map(pro => ({...pro,line: nameLine}));
 }
 
 const heridasAll= addLine( heridas, 'heridas');
-const nefrologiaAll= addLine( nefrologia, 'nefrologia');
+const oncologiaAll= addLine( oncologia, 'oncologia');
 const urologiaAll= addLine( urologia, 'urologia');
+const diabetesAll= addLine(diabetes, 'diabetes');
+const neurologiaAll = addLine(neurologia, 'neurologia');
+const cardiologiaAll = addLine(cardiologia, 'cardiologia');
+const gastroenterologiaAll = addLine(gastroenterologia, 'gastroenterologia');
+const cirugiaAll = addLine(cirugia, 'cirugia');
 
 const all = [
     ...heridasAll,
-    ...nefrologiaAll,
-    ...urologiaAll
+    ...oncologiaAll,
+    ...urologiaAll,
+    ...diabetesAll,
+    ...neurologiaAll,
+    ...cardiologiaAll,
+    ...gastroenterologiaAll,
+    ...cirugiaAll,
 ];
 
 export default all;
