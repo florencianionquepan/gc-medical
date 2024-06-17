4ee0f9124fdc83e1062dfdd5d42c108c0846dfbf
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-17 16:59:41 -0300

    Create TitleComponent.jsx

---
+++ src/products/TitleComponent.jsx
@@ -0,0 +1,11 @@
+
+export const TitleComponent = ({title}) => {
+
+  const titleModified = title === "heridas" ? "Tratamiento de heridas" : title.charAt(0).toUpperCase() + title.slice(1);
+
+  return (
+    <>
+        <h2 className="border-bottom pb-2"> Productos de {titleModified} </h2>
+    </>
+  )
+}
