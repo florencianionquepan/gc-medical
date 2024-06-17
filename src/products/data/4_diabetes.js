21b7f4071df5570aa418d1f5725c419e66a0646b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-17 15:59:05 -0300

    add diabetes info

--- src/products/data/4_diabetes.js
+++ src/products/data/4_diabetes.js
@@ -5,7 +5,7 @@ export const diabetes=[
         provider:'Medtronic',
         image:'diabetes1.jpg',
         hash:'',
-        description:''
+        description:'Ajusta automáticamente la administración de insulina y corrige los niveles de glucosa cada 5 minutos, las 24 horas del día, los 7 días de la semana, sin necesidad de punciones capilares.'
     },
     {
         id:42,
@@ -13,7 +13,7 @@ export const diabetes=[
         provider:'Medtronic',
         image:'diabetes2.jpg',
         hash:'',
-        description:''
+        description:'El sistema MiniMed 670G de Medtronic está destinado a la infusión continua de insulina basal (a índices seleccionables por el usuario) y la administración de bolus de insulina (en cantidades seleccionables por el usuario) para el control de la diabetes mellitus de Tipo 1 en personas a partir de 14 años de edad que necesitan insulina, así como para el control y la determinación de tendencias continuos de los niveles de glucosa en el líquido que se encuentra bajo la piel. El sistema MiniMed 670G incluye la tecnología SmartGuard, que puede programarse para ajustar automáticamente la infusión de insulina basal en función de los valores de glucosa del sensor del monitor continuo de glucosa, y puede suspender la infusión de insulina cuando el valor de glucosa del sensor desciende o se prevé que va a descender por debajo de los valores umbral predefinidos.'
     },
     {
         id:43,
@@ -21,7 +21,7 @@ export const diabetes=[
         provider:'Medtronic',
         image:'diabetes3.jpg',
         hash:'',
-        description:''
+        description:' El sensor revisa los niveles de glucosa cada 5 minutos. Se puede usar en el abdomen, el brazo o en el glúteo, dependiendo de su edad. El transmisor se conecta con el sensor y envía información de la glucosa a su teléfono inteligente.'
     },
     {
         id:44,
@@ -29,7 +29,7 @@ export const diabetes=[
         provider:'Medtronic',
         image:'diabetes4.jpg',
         hash:'',
-        description:''
+        description:'i-Port Advance es un pequeño puerto de inyección que permite la inserción de insulina sin tener que pincharte en la piel con cada inyección. Fácil de llevar y fácil de usar. El puerto puede llevarse durante 3 días en todas las actividades normales, como ducharse, bañarse o hacer deporte.'
     },
     {
         id:45,
@@ -37,7 +37,7 @@ export const diabetes=[
         provider:'Medtronic',
         image:'diabetes5.jpg',
         hash:'',
-        description:''
+        description:'Los reservorios MiniMed™ han sido diseñados teniendo en cuenta tu seguridad para ayudar a que el llenado sea un proceso cómodo. Llenado del reservorio rápido, eficiente y fácil de manejar: No sé requiere montaje, el reservorio está listo para usar. Menos riesgo de lesiones: A diferencia de los reservorios luer, no se necesita manipular una aguja extra, está incorporada en el protector de transferencia. Llenado del reservorio seguro, estable y fácil: El protector de transferencia encaja perfectamente en el vial de insulina. El reservorio MiniMed™ está disponible en tamaño de 1.8 ml y 3 ml para cubrir tus necesidades diarias de insulina.'
     },
     {
         id:46,
@@ -45,7 +45,7 @@ export const diabetes=[
         provider:'Medtronic',
         image:'diabetes6.jpg',
         hash:'',
-        description:''
+        description:'El equipo Quick Set™ combina facilidad de uso con comodidad. También puedes utilizar el insertador Quick-Serter™ para una inserción más consistente. Es adecuado para adultos de complexión media a gruesa y niños. Cánula suave de 90° y profundidad de inserción constante para una absorción óptima de la insulina. Cómodo, inserción posible también con Quick-Serter™. Desconexión cómoda en el punto de infusión.'
     },
     {
         id:47,
@@ -53,7 +53,7 @@ export const diabetes=[
         provider:'Medtronic',
         image:'diabetes7.jpg',
         hash:'',
-        description:''
+        description:'Este equipo te permite ajustar el ángulo de inserción de 20-45° para encontrar el que mejor se adapte a tus necesidades. El insertador MiniMed™ Sil-Serter™ puede utilizarse para una inserción más controlada. También es adecuado para mujeres embarazadas a partir del final del segundo trimestre, cuando el tejido abdominal empieza a tensarse.Cánula de teflón suave. Ángulo de inserción variable de 20 a 45°. Inserción también posible con el insertdor Sil-Serter™.'
     },
 
 ];
