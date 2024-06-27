ef2385e905cc47cbb8361ef294cba1a5a8aa5e62
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-26 21:23:13 -0300

    fix responsive and add dinamic data

--- src/products/lines/LineComponent.jsx
+++ src/products/lines/LineComponent.jsx
@@ -1,16 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import './lines.css';
 
 
-export const LineComponent = ({linea}) => {
+export const LineComponent = ({linea, imagePath, link}) => {
+  const imgRef = useRef(null);
+  const [imgHeight, setImgHeight] = useState(0);
+
+  const handleImageLoad = () => {
+    if (imgRef.current && window.innerWidth>=768 ) {
+      setImgHeight(imgRef.current.clientHeight);
+    }
+  };
+
   return (
     <>
-        <div className='row m-5 justify-content-center'>
-            <div className='col-8 col-md-5 col-lg-3'>
-                <img alt="imagen de linea" src={'/img/cardio.png'} className="rounded"/>
+        <div className='row m-5 justify-content-center align-items-center'>
+            <div className='col-8 col-sm-6 col-md-5 col-lg-3 d-flex justify-content-center rounded p-0 mx-2 me-md-3 me-lg-4'>
+                <img 
+                alt="imagen de linea" 
+                src={imagePath} 
+                className="rounded img-fluid w-custom"
+                ref={imgRef}
+                onLoad={handleImageLoad}
+                />
             </div>
-            <div className="col-8 p-2 col-md-7 col-lg-7 bg-celeste rounded text-white">
-                <h2> {linea} </h2>
-                <p className="d-none d-md-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum soluta ab illum obcaecati possimus voluptate quae aliquid, aliquam placeat? Pariatur officia voluptate aut? Inventore voluptas, aut illum recusandae consectetur excepturi.</p>
-                <button className="btn btn-secondary"> Ver Productos </button>
+            <div className="col-8 col-sm-6 col-md-6 col-lg-7 bg-celeste rounded text-dark text-center text-sm-start p-2 py-sm-4 py-md-2" 
+                  style={{ minHeight: imgHeight }}>
+                <h2 className="mt-3"> {linea} </h2>
+                <p className="d-none d-md-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum soluta ab illum obcaecati possimus </p>
+                <div className="text-end me-5">
+                  <Link className="btn btn-secondary" to={link}> Ver Productos </Link>
+                </div>
+                
             </div>
         </div>
     </>
