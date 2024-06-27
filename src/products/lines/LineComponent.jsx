9f0177fb94efd11b508dc5bda8c9758fdfb8cd7e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-26 22:17:53 -0300

    Update LineComponent.jsx

--- src/products/lines/LineComponent.jsx
+++ src/products/lines/LineComponent.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import './lines.css';
 
 
-export const LineComponent = ({linea, imagePath, link}) => {
+export const LineComponent = ({linea, imagePath, link, bg, orderText}) => {
   const imgRef = useRef(null);
   const [imgHeight, setImgHeight] = useState(0);
 
@@ -15,8 +15,8 @@ export const LineComponent = ({linea, imagePath, link}) => {
 
   return (
     <>
-        <div className='row m-5 justify-content-center align-items-center'>
-            <div className='col-8 col-sm-6 col-md-5 col-lg-3 d-flex justify-content-center rounded p-0 mx-2 me-md-3 me-lg-4'>
+        <div className='row m-5 justify-content-around align-items-center'>
+            <div className='col-8 col-sm-6 col-md-5 col-lg-3 rounded p-0 text-xl-center'>
                 <img 
                 alt="imagen de linea" 
                 src={imagePath} 
@@ -25,12 +25,12 @@ export const LineComponent = ({linea, imagePath, link}) => {
                 onLoad={handleImageLoad}
                 />
             </div>
-            <div className="col-8 col-sm-6 col-md-6 col-lg-7 bg-celeste rounded text-dark text-center text-sm-start p-2 py-sm-4 py-md-2" 
+            <div className={`col-8 col-sm-6 col-md-6 col-lg-8 ${bg} ${orderText} rounded text-dark text-center text-sm-start p-2 py-sm-4 py-md-2`}
                   style={{ minHeight: imgHeight }}>
                 <h2 className="mt-3"> {linea} </h2>
                 <p className="d-none d-md-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum soluta ab illum obcaecati possimus </p>
-                <div className="text-end me-5">
-                  <Link className="btn btn-secondary" to={link}> Ver Productos </Link>
+                <div className="text-md-end me-md-5">
+                  <Link className="btn btn-secondary mb-3 mb-md-0" to={link}> Ver Productos </Link>
                 </div>
                 
             </div>
