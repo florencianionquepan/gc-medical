4621714121b8f94271ef3f1e38dac9f66a886ae7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-29 15:06:18 -0300

    create items lines in assets

---
+++ src/assets/helpers/itemsLines.js
@@ -0,0 +1,78 @@
+const items=[
+    { 
+      id: 21,
+      imagePath:'./img/products/heridas1.jpg', 
+      linea: 'Tratamiento de heridas', 
+      text:'',
+      link: '/linea/heridas', 
+      bg:'bg-celeste', 
+      orderText:''
+    },
+    { 
+      id: 22,
+      imagePath:'./img/products/oncologia1.jpg', 
+      linea: 'Oncología', 
+      text:'',
+      link: '/linea/oncologia',
+      bg:'bg-gris', 
+      orderText:'order-md-first'
+    },
+    { 
+      id: 23,
+      imagePath:'./img/products/urologia1.jpg', 
+      linea: 'Urología', 
+      text:'',
+      link: '/linea/urologia',
+      bg:'bg-azul', 
+      orderText:''
+    },
+    { 
+      id: 24,
+      imagePath:'./img/products/diabetes1.webp', 
+      linea: 'Diabetes', 
+      text:'',
+      link: '/linea/diabetes',
+      bg:'bg-celeste', 
+      orderText:'order-md-first'
+    },
+    { 
+      id: 25,
+      imagePath:'./img/products/neurologia1.jpg', 
+      linea: 'Neurología', 
+      text:'',
+      link: '/linea/neurologia',
+      bg:'bg-gris', 
+      orderText:'' 
+    },
+    { 
+      id: 26,
+      imagePath:'./img/products/neurologia2.jpg', 
+      linea: 'Cardiologia', 
+      text:'',
+      link: '/linea/cardiologia',
+      bg:'bg-azul', 
+      orderText:'order-md-first' 
+    },
+    { 
+      id: 28,
+      imagePath:'./img/products/neurologia3.jpg', 
+      linea: 'Gastroenterología', 
+      text:'',
+      link: '/linea/gastroenterologia', 
+      bg:'bg-gris', 
+      orderText:''
+    },
+    { 
+      id: 29,
+      imagePath:'./img/products/neurologia4.jpg', 
+      linea: 'Cirugia General', 
+      text:'',
+      link: '/linea/cirugia',
+      bg:'bg-celeste', 
+      orderText:''
+    },
+];
+
+
+export default items;
+  
