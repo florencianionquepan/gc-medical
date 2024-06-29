7fee6a1e80aaef6fc6ee23f08c38b19292174284
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-29 18:59:24 -0300

    Update HomePage.jsx

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import {BreakpointIndicator} from './../assets/BreakpointIndicator'
 import { Carousel } from './Carousel'
 import { ProductsSection } from './ProductsSection'
 import { ContactSection } from './ContactSection'
 import { AboutSection } from './AboutSection'
+import { CarouselLines } from './CarouselLines'
 
 export const HomePage = () => {
   return (
     <>
+    <BreakpointIndicator/>
     <div className='container-fluid'>
       <Carousel/>
+    </div>
+    <div className='container-lg'>
+      <AboutSection/>
       <ProductsSection/>
       <ContactSection/>
-      <AboutSection/>
     </div>
     
     {/* <div className='container-fluid'>
