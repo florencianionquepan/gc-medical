8aa38c17c8fa12a4e5b4b1f98421c921fc22810d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-06-29 20:22:50 -0300

    Update ContactSection.jsx

--- src/home/ContactSection.jsx
+++ src/home/ContactSection.jsx
@@ -1,19 +1,16 @@
+import { Link } from "react-router-dom"
 import { ComunidadComponent } from "../assets/ComunidadComponent"
 
 export const ContactSection = () => {
   return (
-    <>
-        <div className='p-3 m-5 border border-gris border-5 rounded rozha-one'>
-            <div className='row align-items-center'>
-                <div className='col-12 col-md-6 text-center my-3'>
-                  <h1 className="display-2"> Contacto </h1>
-                  <a className="btn btn-gris btn-lg">Ver mas</a>
-                </div>
-                <div className='col-12 col-md-6 display-6 my-3'>
-                  <ComunidadComponent/>
-                </div>
-            </div>
-        </div>
-    </>
+    <div className='row align-items-center p-3 m-5 border border-gris border-5 rounded rozha-one'>
+      <div className='col-12 col-md-6 text-center my-3'>
+        <h1 className="display-2"> Contacto </h1>
+        <Link className="btn btn-gris btn-lg" to='/contacto'>Ver mas</Link>
+      </div>
+      <div className='col-12 col-md-6 display-6 my-3'>
+        <ComunidadComponent/>
+      </div>
+    </div>
   )
 }
