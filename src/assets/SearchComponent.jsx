802ee4b56ca03135f86b925d77d655e5f684e48d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-05 21:17:54 -0300

    Create SearchComponent.jsx

---
+++ src/assets/SearchComponent.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+export const SearchComponent = ({onSearch}) => {
+
+  const [search, setSearch] = useState("")
+
+  const searcher = (e) => {
+    const searchTerm=e.target.value;
+    setSearch(searchTerm);
+    onSearch(searchTerm);
+  }
+
+
+  return (
+    <div className='row m-5 justify-content-end'>
+        <form className="col-12 col-md-8 col-lg-6 col-xl-4 mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" 
+                    className="form-control" 
+                    placeholder="Buscar por nombre o por linea..." 
+                    aria-label="Search"
+                    value={search}
+                    onChange={searcher}/>
+        </form>
+    </div>
+  )
+}
