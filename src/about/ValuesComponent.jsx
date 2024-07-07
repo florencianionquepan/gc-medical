83b6bc3f1538d3f88d59accc91d55c83c3793ed8
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-07 13:14:30 -0300

    Update ValuesComponent.jsx

--- src/about/ValuesComponent.jsx
+++ src/about/ValuesComponent.jsx
@@ -4,7 +4,7 @@ export const ValuesComponent = ({title,text}) => {
   return (
     <div className='col-10 col-md-2 border rounded border-gris border-3 p-2 mx-2'>
         <div className="circle-container border-gris">
-            <h5 className="rozha-one circle-title">{title}</h5>
+            <h5 className="rozha-one circle-title fs-6">{title}</h5>
         </div>
         <div>
             <p>{text}</p>
