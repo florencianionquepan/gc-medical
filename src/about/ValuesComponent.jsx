2cfd9d56820937edb830ea319e188372cd559544
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-09 14:13:54 -0300

    fix styles

--- src/about/ValuesComponent.jsx
+++ src/about/ValuesComponent.jsx
@@ -2,12 +2,12 @@
 
 export const ValuesComponent = ({title,text}) => {
   return (
-    <div className='col-10 col-md-2 border rounded border-gris border-3 p-2 mx-2'>
+    <div className='col-10 col-sm-8 col-md-4 col-xxl-2 border rounded border-gris border-5 p-2 m-2'>
         <div className="circle-container border-gris">
             <h5 className="rozha-one circle-title fs-6">{title}</h5>
         </div>
         <div>
-            <p>{text}</p>
+            <p className="mb-1">{text}</p>
         </div>
      
     </div>
