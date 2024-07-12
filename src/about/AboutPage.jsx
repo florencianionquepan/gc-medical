721b66fe4d9f1c1bf2f0715d5347b63b1f6fca77
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-12 17:35:37 -0300

    Update AboutPage.jsx

--- src/about/AboutPage.jsx
+++ src/about/AboutPage.jsx
@@ -1,63 +1,14 @@
 import { AboutComponent } from "./AboutComponent"
 import { ValuesComponent } from "./ValuesComponent";
-import {BreakpointIndicator} from "./../assets/utils/BreakpointIndicator"
+import {info} from "./data/info";
 import './about.css';
 
-const info=[
-  {
-    id:1,
-    title:'Misión',
-    text:'Somos una empresa comprometida en alcanzar la completa satisfacción de nuestros clientes cuidando la calidad de nuestros productos y nuestros servicios, para poder así mejorar la calidad de vida de nuestros clientes.'
-  },
-  {
-    id:2,
-    title:'Visión',
-    text:'Ser una empresa líder en el mercado local de distribución de productos médicos proyectada hacia el futuro, con una tendencia a desarrollarse de acuerdo a las oportunidades de negocios que se presenten.'
-  },
-  {
-    id:3,
-    title:'Valores',
-    texts:[
-      {
-        id:31,title:'Confianza',text:`Construimos relaciones basadas en la consideración personal y profesional, 
-                                      brindando respaldo y seguridad a nuestros clientes.`
-      },
-      {
-        id:32,title:'Compromiso',text:`Nos comprometemos a orientarnos hacia la mejora continua, promovemos la interacción, 
-                                      el esfuerzo y la contribución de todo nuestro personal hacia el logro 
-                                      de nuevos resultados y la satisfacción de nuestros clientes.`
-      },
-      {
-        id:33,title:'Respeto',text:`Cuidamos nuestros vínculos con los demás, atendiendo con eficiencia, 
-                                      cortesía y servicio los requerimientos que nos sean solicitados`
-      },
-      {
-        id:34, title:'Integridad', text:'Asumimos una conducta honesta y coherente.'
-      }],
-  },
-  {
-    id:4,
-    title:'Nuestra historia',
-    text:`Somos una empresa dedicada a la provisión de prótesis e insumos médicos de mediana y alta complejidad, 
-          representante de marcas líderes en las diversas especialidades. En el año 1990 iniciamos Guillermo Consellón productos médicos 
-          y en el año 2005 cambiamos nuestra razón social pasando a ser GC medical SRL. 
-          Inicialmente comercializamos nuestros productos en la región sur de la provincia de Buenos Aires 
-          y en la actualidad nos expandimos con algunas líneas de productos a las provincias de Río Negro y Neuquén`
-  },
-  {
-    id:5,
-    title:'Servicios u otro titulo?',
-    text:`Para brindar un mejor servicio contamos en nuestros puntos de venta con stock permanente de materiales de urgencia
-          y aseguramos una pronta entrega en todos nuestros productos.
-          El personal se encuentra altamente capacitado para brindar asistencia y asesoramiento en las diversas áreas (aspectos técnicos administrativos y legales).
-          Buscamos brindar soluciones para que la cobertura del producto sea posible.
-          Trabajamos para mejorar la calidad de vida de los pacientes.`
-  }
-]
 
 export const AboutPage = () => {
+
   return (
     <div className='container-xl'>
+      <h2 className="border-bottom pb-2 text-md-start mt-5 mx-5"> Sobre nuestra empresa </h2>
       {/* <BreakpointIndicator/> */}
         <div className='row justify-content-center m-3'>
           {/* Mision */}
