d8a76040ce948fd0c565345beb0af3afddfe4351
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-12 18:49:02 -0300

    Update AboutSection.jsx

--- src/home/AboutSection.jsx
+++ src/home/AboutSection.jsx
@@ -2,23 +2,15 @@ import { Link } from 'react-router-dom'
 
 export const AboutSection = () => {
   return (
-    <div className='row align-items-center m-5 border border-gris border-5 rounded rozha-one bg-gris'>
-      <div className='col-12 col-md-7 text-center my-3 my-md-0'>
+    <div className='row align-items-center m-5 border border-gris border-5 rounded rozha-one'>
+      <div className='col-12 col-md-7 text-center my-3 my-md-0 order-2'>
         <img className='img-fluid rounded' src={'/img/nosotros.webp'}/>
       </div>
-      <div className='col-12 col-md-5 display-6 text-center py-2 bg-body rounded'>
+      <div className='col-12 col-md-5 display-6 text-center py-2 bg-body rounded order-1'>
         <h1 className="display-2 text-md-start mb-0"> Sobre </h1>
         <h1 className="display-2 text-md-end mt-0"> nosotros </h1>
         <Link className="btn btn-gris btn-lg" to='/nosotros'>Ver mas</Link>
       </div>
-      {/* <div className='col-12 position-relative'>
-        <img className='img-fluid rounded' src={'/img/nosotros.webp'} alt="Nosotros" />
-        <div className='position-absolute bottom-0 end-0 rounded text-center p-3 bg-white bg-opacity-75 me-3 mb-3'>
-          <h1 className="display-2 text-md-start mb-0"> Sobre </h1>
-          <h1 className="display-2 text-md-end mt-0"> nosotros </h1>
-          <Link className="btn btn-gris btn-lg mt-3" to='/nosotros'>Ver más</Link>
-        </div>
-      </div> */}
     </div>
   )
 }
