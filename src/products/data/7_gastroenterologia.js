f98de7afb284aef70ee4cfc03d918ebfdc52ad0d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-13 20:02:24 -0300

    Update 7_gastroenterologia.js

--- src/products/data/7_gastroenterologia.js
+++ src/products/data/7_gastroenterologia.js
@@ -1,3 +1,98 @@
 export const gastroenterologia=[
-    
+    {
+        id:71,
+        name:'Hemoclip SureClip',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'',
+        description:''
+    },
+    {
+        id:72,
+        name:'Aguja De Ecoendoscopía Areus',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LZQm9jxu-;aykCt7axaz~qWBMxfQ',
+        description:''
+    },
+    {
+        id:73,
+        name:'Papilotomo Triple Lumen',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:74,
+        name:'Asa De Polipectomía Fría LesionHunter',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'',
+        description:''
+    },
+    {
+        id:75,
+        name:'Asa De Polipectomía Caliente',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LZQm9jxu-;aykCt7axaz~qWBMxfQ',
+        description:''
+    },
+    {
+        id:76,
+        name:'Aguja De Inyección',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:77,
+        name:'Balón De Extracción De Cálculos Biliares',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:78,
+        name:'Balón De Dilatación',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:79,
+        name:'Cuchillo ESD GoldKnife',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:790,
+        name:'Stent Esofágico Antirreflujo',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:791,
+        name:'Stent Biliar Plástico Doble Pigtail',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:792,
+        name:'Pinza De Biopsia TechBite',
+        provider:'Promedon',
+        image:'default.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    }
 ]
