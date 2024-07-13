565b5da62b864da9fabda579ddf751b610d91810
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-13 18:54:30 -0300

    update images and add blurhash

--- src/products/data/2_oncologia.js
+++ src/products/data/2_oncologia.js
@@ -4,7 +4,7 @@ export const oncologia=[
         name:'Aguja Poly Port Safe',
         provider:'Lexel SRL',
         image:'oncologia1.webp',
-        hash:'',
+        hash:'LHPZfWKPRPVs^*%MkBRQ?^MdtR%M',
         description:"Aguja Poly Port Safe (Poly Port SF), con tubo de acero de pared ultra delgada y punta tipo Huber, posee un cabezal que permite la segura inserción en el Portal y la fijación al cuerpo del paciente para infusiones prolongadas."
     },
     {
@@ -12,7 +12,7 @@ export const oncologia=[
         name:'Port Implantable de Titanio',
         provider:'Lexel SRL',
         image:'oncologia2.webp',
-        hash:'',
+        hash:'L8S?DVIU%M~q?IxuRjM{_3%MM_9F',
         description:"Es un sistema implantable para la administración de fármacos construido en titanio. Esto hace a este portal extra-liviano, de máxima resistencia y absoluta biocompatibilidad. Su septum autosellante de silicona, permite mas de 1000 punciones con agujas de punta tipo Huber (G21) sin que se produzcan pérdidas. Existen tres diferentes tamaños: Adulto, Pediatrico y el exclusivo modelo Neonatal. El cateter construido en caucho de silicona es radiopaco especialmente diseñado para uso venoso (9, 7 y 5,5 Fr) y peritoneal."
     },
 ]
--- src/products/data/4_diabetes.js
+++ src/products/data/4_diabetes.js
@@ -4,7 +4,7 @@ export const diabetes=[
         name:'Sistema MiniMed 780G',
         provider:'Medtronic',
         image:'diabetes1.webp',
-        hash:'',
+        hash:'LyPGmfof~qRjt7t7j[Rj_3j[D%fk',
         description:'Ajusta automáticamente la administración de insulina y corrige los niveles de glucosa cada 5 minutos, las 24 horas del día, los 7 días de la semana, sin necesidad de punciones capilares.'
     },
     /* {
@@ -20,7 +20,7 @@ export const diabetes=[
         name:'Guardian Sensor Transmisor',
         provider:'Medtronic',
         image:'diabetes3.webp',
-        hash:'',
+        hash:'LLS6Stxu~qE1xuayWBof~pRj9F%M',
         description:' El sensor revisa los niveles de glucosa cada 5 minutos. Se puede usar en el abdomen, el brazo o en el glúteo, dependiendo de su edad. El transmisor se conecta con el sensor y envía información de la glucosa a su teléfono inteligente.'
     },
     {
@@ -28,7 +28,7 @@ export const diabetes=[
         name:'I Port Advance',
         provider:'Medtronic',
         image:'diabetes4.webp',
-        hash:'',
+        hash:'LhO#1e}?^j9]NfxtoIRk~AEMIpxt',
         description:'i-Port Advance es un pequeño puerto de inyección que permite la inserción de insulina sin tener que pincharte en la piel con cada inyección. Fácil de llevar y fácil de usar. El puerto puede llevarse durante 3 días en todas las actividades normales, como ducharse, bañarse o hacer deporte.'
     },
     {
@@ -36,7 +36,7 @@ export const diabetes=[
         name:'Reservorio MiniMed',
         provider:'Medtronic',
         image:'diabetes5.webp',
-        hash:'',
+        hash:'LHSY{s%2~pIWRpxtt3IV-;WCD%t6',
         description:'Los reservorios MiniMed™ han sido diseñados teniendo en cuenta tu seguridad para ayudar a que el llenado sea un proceso cómodo. Llenado del reservorio rápido, eficiente y fácil de manejar: No sé requiere montaje, el reservorio está listo para usar. Menos riesgo de lesiones: A diferencia de los reservorios luer, no se necesita manipular una aguja extra, está incorporada en el protector de transferencia. Llenado del reservorio seguro, estable y fácil: El protector de transferencia encaja perfectamente en el vial de insulina. El reservorio MiniMed™ está disponible en tamaño de 1.8 ml y 3 ml para cubrir tus necesidades diarias de insulina.'
     },
     {
@@ -44,7 +44,7 @@ export const diabetes=[
         name:'Quick Set',
         provider:'Medtronic',
         image:'diabetes6.webp',
-        hash:'',
+        hash:'L5S~x4?b%MIA~qM{Rjxv-;Rjof%M',
         description:'El equipo Quick Set™ combina facilidad de uso con comodidad. También puedes utilizar el insertador Quick-Serter™ para una inserción más consistente. Es adecuado para adultos de complexión media a gruesa y niños. Cánula suave de 90° y profundidad de inserción constante para una absorción óptima de la insulina. Cómodo, inserción posible también con Quick-Serter™. Desconexión cómoda en el punto de infusión.'
     },
     {
@@ -52,7 +52,7 @@ export const diabetes=[
         name:'Set de infusión Silhouette',
         provider:'Medtronic',
         image:'diabetes7.webp',
-        hash:'',
+        hash:'L3T9Isxu_N%Mxaof-;RP_3ofaxRj',
         description:'Este equipo te permite ajustar el ángulo de inserción de 20-45° para encontrar el que mejor se adapte a tus necesidades. El insertador MiniMed™ Sil-Serter™ puede utilizarse para una inserción más controlada. También es adecuado para mujeres embarazadas a partir del final del segundo trimestre, cuando el tejido abdominal empieza a tensarse.Cánula de teflón suave. Ángulo de inserción variable de 20 a 45°. Inserción también posible con el insertdor Sil-Serter™.'
     },
 
