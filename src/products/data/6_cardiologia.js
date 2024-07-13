97db5e2bba3fa17af4d768304c37c53b089677f2
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-13 19:28:34 -0300

    add cardiology images

--- src/products/data/6_cardiologia.js
+++ src/products/data/6_cardiologia.js
@@ -1,3 +1,48 @@
 export const cardiologia=[
+    //61 y 62 van juntos
+    {
+        id:61,
+        name:'Marcapasos Uni/Bicameral Attesta DR/SR',
+        provider:'Medtronic',
+        image:'cardiologia1.webp',
+        hash:'LEEf+;W=00Z~Zf~USi5800oz^%V@',
+        description:''
+    },
+    {
+        id:63,
+        name:'Resincronizador cardiaco',
+        provider:'Medtronic',
+        image:'cardiologia3.webp',
+        hash:'LZQm9jxu-;aykCt7axaz~qWBMxfQ',
+        description:''
+    },
+    {
+        id:64,
+        name:'Resincronizador con desfibrilador',
+        provider:'Medtronic',
+        image:'cardiologia4.webp',
+        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        description:''
+    },
+    {
+        id:65,
+        name:'Marcapasos Micra',
+        provider:'Medtronic',
+        image:'cardiologia5.webp',
+        hash:'LWQ0U8fP%Mt7?bofayay~qt7M{Ri',
+        description:`Dispositivo cardíaco programable que monitoriza y regula la frecuencia cardíaca del paciente 
+                    mediante la administración de estimulación antibradicardia de respuesta en frecuencia en el 
+                    ventrículo derecho y de sincronía AV basándose en la detección mecánica de la actividad auricular.
+                    El dispositivo detecta tanto la actividad eléctrica como la actividad mecánica del corazón del
+                    paciente mediante electrodos de detección y estimulación y un acelerómetro integrado en
+                    una cápsula de titanio en miniatura. Monitoriza el corazón en busca de bradicardia y
+                    sincronía AV. Además, proporciona las funciones siguientes para los pacientes:
+                    • El dispositivo responde a la bradicardia con la administración de una terapia de
+                    estimulación basada en los parámetros de estimulación programados.
+                    • El dispositivo proporciona sincronía AV basándose en la actividad mecánica detectada
+                    en la aurícula.
+                    • El dispositivo proporciona información de diagnóstico y monitorización para evaluar el
+                    rendimiento del dispositivo y proporcionar el mejor cuidado posible al paciente.`
+    },
     
 ]
