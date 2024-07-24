ad7013f58db0148ab99efd74469550c2d7aa1ee9
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-24 19:44:14 -0300

    add gastro info

--- src/products/data/7_gastroenterologia.js
+++ src/products/data/7_gastroenterologia.js
@@ -5,7 +5,15 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro1.webp',
         hash:'L6S$ovIU~q.8~q?bWB9F-;D%4n%M',
-        description:''
+        description:`Los clips deben ser confiables. 
+                    Necesitan ser precisos. Deben permitirle la flexibilidad de reposicionar o rotar tanto como sea necesario para brindar mejores resultados. 
+                    El posicionamiento preciso antes del despliegue puede reducir tanto el tiempo del procedimiento 
+                    como la cantidad de clips necesarios para lograr la hemostasia. 
+                    SureClip logra esto mediante el diseño que ofrece una capacidad de reposicionamiento excepcional 
+                    y una rotación confiable antes del despliegue, en varias posiciones de alcance. 
+                    El vástago corto de SureClip ayuda a la colocación en luz estrecha y mejora la visibilidad. 
+                    El diseño de clip patentado proporciona una implementación confiable, 
+                    puede mejorar la retención y ofrece una selección de tamaños de mandíbula.`
     },
     {
         id:72,
@@ -13,7 +21,9 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro2.webp',
         hash:'L6S$ovIU~q.8~q?bWB9F-;D%4n%M',
-        description:''
+        description:`La aguja Areus ™ FNA cuenta con una punta de bisel de doble filo alargado para ayudar en la penetración precisa en el área objetivo, 
+                    proporcionando un rendimiento constante para la citología. 
+                    Las agujas EUS están disponibles en 19ga, 22ga y 25ga e incluyen una jeringa Vacuum Lock de 20cc y una llave de paso.`
     },
     {
         id:73,
@@ -21,15 +31,18 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro3.webp',
         hash:'L8SPb300%M?a%L-;?HWC~qxt9F9G',
-        description:''
+        description:`El Triple-Lumen esfinterótomo estéril está destinado a ser utilizado en la canulación selectiva de los conductos biliares 
+                    y el esfinterotomía de la papila de Vater y / o el esfínter de Oddi. 
+                    La esfinterotomía biliar es un procedimiento en el cual se hace una incisión pequeña en la abertura del conducto biliar 
+                    para permitir que drenen los cálculos biliares pequeños.`
     },
     {
         id:74,
-        name:'Asa De Polipectomía Fría LesionHunter',
+        name:'Asa De Polipectomía Fría LesionHunter?',
         provider:'Promedon',
         image:'gastro4.webp',
         hash:'LCR{+1of?aR*?at7t7Rj~ot7axWB',
-        description:''
+        description:'Ver si falta info o no va'
     },
     {
         id:75,
@@ -37,15 +50,18 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro5.webp',
         hash:'L6T9LyWBWBof?w%Mt8ofbdM{M{Rj',
-        description:''
+        description:`Cada asa está hecha con un alambre trenzado (multifilamento) especial que proporciona una fuerte fuerza radial, 
+                    estabilidad y propiedades de corte precisas. 
+                    La conexión de alimentación se monta independientemente de la diapositiva del anillo de dedo en el extremo distal de la pieza de mano, 
+                    evitando que el cable de alimentación se mueva cuando la caja se abre y se cierra mejorando el manejo.`
     },
     {
         id:76,
-        name:'Aguja De Inyección',
+        name:'Aguja Flexible VANTRIS',
         provider:'Promedon',
         image:'gastro6.webp',
         hash:'L2TI,at7%M_3?bxuRjIUR%ofIUM|',
-        description:''
+        description:'VANTRIS es una sustancia biocompatible, no absorbible y sintética utilizada para el tratamiento endoscópico del reflujo vesicoureteral (VUR) en la población pediátrica1. VANTRIS está compuesto por partículas de copolímero de poliacrilato polialcohol (PPC) sumergidas en un vehículo y se presenta en una jeringa de 1 ml, lista para ser inyectada fácilmente y de forma manual mediante agujas de calibre 22 o 23 a través del procedimiento de inyección suburetral (técnica STING).'
     },
     {
         id:77,
@@ -53,7 +69,14 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro7.webp',
         hash:'L7S?DWNH~p?a-pt6bHR*~Wt79FIo',
-        description:''
+        description:`El balón de extracción de cálculos biliares estéril es un dispositivo médico que cuenta en su parte proximal con tres conectores luer-lock 
+                    que se utilizan para el inflado del balón, pasaje de cable guía y para inyección de líquidos 
+                    (usualmente líquido de contraste). Estos conectores se unen con los 3 lúmenes que forman parte del catéter triple lumen. 
+                    El catéter triple lumen en su extremo distal tiene un balón de látex natural de diferentes diámetros y longitud. 
+                    El balón de extracción es un producto de uso único, utilizado para la extracción endoscópica de cálculos biliares. 
+                    También puede ser utilizado para la inyección de medio de contraste mientras se obstruye el ducto biliar con el balón. 
+                    El balón puede proveerse con tres jeringas de policarbonato con tope de silicona, las cuales aseguran 3 diámetros diferentes de inflado del balón. 
+                    Estas jeringas cumplen la función de llenar el balón con aire y no inyectan fluido al paciente ni tienen contacto con el mismo.`
     },
     {
         id:78,
@@ -61,7 +84,10 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro8.webp',
         hash:'L5S~x6-p?aog?bs:M{WB~VNGIoxt',
-        description:''
+        description:`El globo Pebax ™ presenta poca o ninguna cintura. 
+                    El material transparente y el diseño del globo permiten la visualización durante todo el procedimiento. 
+                    El alambre fijo presenta una punta suave y cónica que ayuda a la colocación del globo y protege la mucosa. 
+                    El catéter 7 Fr combina la facilidad de uso con un rápido inflado y desinflado.`
     },
     {
         id:79,
@@ -69,15 +95,20 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro9.webp',
         hash:'LDSY{qM{xu-;xuRjofxu~qxuofM{',
-        description:''
+        description:`El GOLDKNIFE de MICRO-TECH permite realizar disecciones submucosas endoscópicas eficaces y fiables. 
+                    Gracias a la combinación de irrigación a través del bisturí y la electrocirugía, todos los pasos de trabajo 
+                    – marcado, inyección, disección y coagulación, pueden realizarse sin cambiar de instrumento.`
     },
     {
         id:790,
-        name:'Stent Esofágico Antirreflujo',
+        name:'Stent Duodenal Recubierto',
         provider:'Promedon',
         image:'gastro10.webp',
         hash:'LbRMh{Rj~q%MkBM{WB-p%MWBIUof',
-        description:''
+        description:`Sus extremos esféricos se adaptan perfectamente a la anatomía del duodeno, garantizando máxima estabilidad posicional. 
+                    Disponibilidad de stents con y sin recubrimiento para las distintas indicaciones. 
+                    El stent viene precargado en un introductor TTS estándar, de manera que la colocación es posible a través del canal de trabajo del endoscopio. 
+                    Tanto el stent como el introductor contienen marcas radiopacas que aseguran una óptima visión radioscópica.`
     },
     {
         id:791,
@@ -85,7 +116,12 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro11.webp',
         hash:'L8S$u?t7%2xuX9%LoeM|~VxaIoM|',
-        description:''
+        description:`El catéter de drenaje biliar sin introductor se utiliza para drenar los conductos biliares obstruidos.  
+                        La bilis se produce en el hígado y se almacena en la vesícula biliar, de la que puede ser liberada hacia el interior del intestino delgado para ayudar durante la digestión. 
+                        Si los conductos biliares se bloquean, se podría producir una inflamación o infección, conocida también como colecistitis.  
+                        De aquí la importancia de drenaje de biliar, como método para evitar las complicaciones antes mencionadas. 
+                        El drenaje biliar es un método para diagnóstico y tratamiento de enfermedades que producen obstrucción de la vía biliar. 
+                        Pueden identificarse la causa y el sitio de la obstrucción, resolviéndola en el mismo acto quirúrgico. `
     },
     {
         id:792,
@@ -93,6 +129,11 @@ export const gastroenterologia=[
         provider:'Promedon',
         image:'gastro12.webp',
         hash:'LBSija%1-:oh-:RlRloI~oWCM{j?',
-        description:''
+        description:`Las pinzas MICRO-TECH TechBite ™ están equipadas con un diseño especial. 
+                    Las copas garantizan capacidad de muestra significativa y se caracterizan para un rendimiento de corte preciso.
+                    El modelo incluye pinzas con púas y sin púas, así como bordes dentados y lisos. 
+                    La manija proporciona control ideal y una perfecta sensación táctil. 
+                    Para proteger el canal y reducir la fricción por deslizamiento, la línea TechBite ™ está completamente recubierto.
+                    `
     }
 ]
