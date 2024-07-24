6e7b007c1893d77fb80fb6915f1bcb7a95dca449
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-24 19:05:08 -0300

    add gastro images hashes

--- src/products/data/7_gastroenterologia.js
+++ src/products/data/7_gastroenterologia.js
@@ -3,96 +3,96 @@ export const gastroenterologia=[
         id:71,
         name:'Hemoclip SureClip',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LZQm9jxu-;aykCt7axaz~qWBMxfQ',
+        image:'gastro1.webp',
+        hash:'L6S$ovIU~q.8~q?bWB9F-;D%4n%M',
         description:''
     },
     {
         id:72,
         name:'Aguja De Ecoendoscopía Areus',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LZQm9jxu-;aykCt7axaz~qWBMxfQ',
+        image:'gastro2.webp',
+        hash:'L6S$ovIU~q.8~q?bWB9F-;D%4n%M',
         description:''
     },
     {
         id:73,
         name:'Papilotomo Triple Lumen',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro3.webp',
+        hash:'L8SPb300%M?a%L-;?HWC~qxt9F9G',
         description:''
     },
     {
         id:74,
         name:'Asa De Polipectomía Fría LesionHunter',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LZQm9jxu-;aykCt7axaz~qWBMxfQ',
+        image:'gastro4.webp',
+        hash:'LCR{+1of?aR*?at7t7Rj~ot7axWB',
         description:''
     },
     {
         id:75,
         name:'Asa De Polipectomía Caliente',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LZQm9jxu-;aykCt7axaz~qWBMxfQ',
+        image:'gastro5.webp',
+        hash:'L6T9LyWBWBof?w%Mt8ofbdM{M{Rj',
         description:''
     },
     {
         id:76,
         name:'Aguja De Inyección',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro6.webp',
+        hash:'L2TI,at7%M_3?bxuRjIUR%ofIUM|',
         description:''
     },
     {
         id:77,
         name:'Balón De Extracción De Cálculos Biliares',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro7.webp',
+        hash:'L7S?DWNH~p?a-pt6bHR*~Wt79FIo',
         description:''
     },
     {
         id:78,
         name:'Balón De Dilatación',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro8.webp',
+        hash:'L5S~x6-p?aog?bs:M{WB~VNGIoxt',
         description:''
     },
     {
         id:79,
         name:'Cuchillo ESD GoldKnife',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro9.webp',
+        hash:'LDSY{qM{xu-;xuRjofxu~qxuofM{',
         description:''
     },
     {
         id:790,
         name:'Stent Esofágico Antirreflujo',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro10.webp',
+        hash:'LbRMh{Rj~q%MkBM{WB-p%MWBIUof',
         description:''
     },
     {
         id:791,
         name:'Stent Biliar Plástico Doble Pigtail',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro11.webp',
+        hash:'L8S$u?t7%2xuX9%LoeM|~VxaIoM|',
         description:''
     },
     {
         id:792,
         name:'Pinza De Biopsia TechBite',
         provider:'Promedon',
-        image:'default.webp',
-        hash:'LWRfh2t7_NRjozt7aeRP.8j[IUWB',
+        image:'gastro12.webp',
+        hash:'LBSija%1-:oh-:RlRloI~oWCM{j?',
         description:''
     }
 ]
