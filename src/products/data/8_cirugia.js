70f0208e143f3c07210b4a7d87d747f717d5e35f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-24 19:23:13 -0300

    add surgery info

--- src/products/data/8_cirugia.js
+++ src/products/data/8_cirugia.js
@@ -1,3 +1,34 @@
 export const cirugia=[
-    
+    {
+        id:81,
+        name:'Engrapadora Para Tratamiento De Prolapso Hemorroidal',
+        provider:'Reach surgical',
+        image:'cirugia1.webp',
+        hash:'L6S$ovIU~q.8~q?bWB9F-;D%4n%M',
+        description:'El dispositivo Reach PPH33 proporciona 2 filas de grapa de titanio intercaladas con un diámetro de 33mm diseñado para prolapso hemorroidal. '
+    },
+    {
+        id:82,
+        name:'Pinza Ultrasónica Laparoscópica Con Punta Curva',
+        provider:'Reach surgical',
+        image:'cirugia2.webp',
+        hash:'L6S$ovIU~q.8~q?bWB9F-;D%4n%M',
+        description:'La pinza ultrasónica SOUND REACH proporciona rapidez, seguridad, facilidad y flexibilidad en las cirugías donde la energía avanzada es necesaria. '
+    },
+    {
+        id:83,
+        name:'Engrapadora Circular Cabezal Fijo',
+        provider:'Reach surgical',
+        image:'cirugia3.webp',
+        hash:'L6S$ovIU~q.8~q?bWB9F-;D%4n%M',
+        description:'La engrapadora Circular REACH ACS otorga seguridad y confianza en las anastomosis. Esto gracias a un diseño ergonómico, más cantidad de grapas y visor para controlar la altura en el cierre de las mismas.'
+    },
+    {
+        id:84,
+        name:'Endograpador Endo III Articulado',
+        provider:'Reach surgical',
+        image:'cirugia4.webp',
+        hash:'LGSijY-:xuM{bHoffRj[~pIVM{%2',
+        description:'La endograpadora Endo III cuenta con un novedoso sistema de articulación libre y una alta variedad de colores de grapas con cuchilla incluida para todo tipo de cirugías, alcanzando una hemostasia eficaz gracias a la compresión superior de su mandíbula y creando un engrapado uniforme en sus 6 líneas de grapas.'
+    },
 ]
