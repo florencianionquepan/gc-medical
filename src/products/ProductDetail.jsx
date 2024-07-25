fcd8d676836979e4f7e05cff8e07a2b6a4b86ca8
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-07-24 21:16:37 -0300

    fix styles and add back button

--- src/products/ProductDetail.jsx
+++ src/products/ProductDetail.jsx
@@ -1,7 +1,8 @@
 import { useMemo } from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
-import {getProductById} from './helpers/getProductById';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { getProductById } from './helpers/getProductById';
 import { TitleProductComponent } from './TitleProductComponent';
+import { ImageComponent } from '../assets/utils/ImageComponent';
 
 export const ProductDetail = () => {
 
@@ -9,33 +10,57 @@ export const ProductDetail = () => {
   const navigate = useNavigate();
   const product = useMemo( ()=> getProductById(id), [id]);
 
-  const imagePath = '/img/products/'.concat(product.image);
+  const imagePath = product? '/img/products/'.concat(product.image): '';
 
   const splitDescription = (description) => {
-    const parts = description.split('.'); 
-    return parts.map((part, index) => (
+    const parts = description.split('.');
+    const filteredParts = parts.filter(part => part.trim() !== ''); 
+    return filteredParts.map((part, index) => (
       <p key={index}>{part.trim()}.</p> 
     ));
   };
 
+  const onNavigateBack = () =>{
+    navigate(-1);
+  }
+
+  if(!product){
+    return <Navigate to='/productos'/>
+  }
 
   return (
-    <div className='container-md'>
-        <div className='row m-5'>
-          <TitleProductComponent title={product.name}/>
-        </div>
-        <div className='row m-5'>
-          <div className='col-6'>
-            <img
-              src={imagePath}
-              alt={product.name}
-              className="rounded-lg border border-gray-500 shadow-violet p-2 m-2 img-responsive img-detail"
-            />
+    <div className='container-md' style={{minHeight:'60vh'}}>
+        <div className='row mx-5 mt-5 rozha-one border-bottom pb-2'>
+          <div className='col-auto'>
+            <button className='btn btn-gris' 
+            onClick={()=>onNavigateBack()}>
+              <i className="bi bi-arrow-return-left"></i>
+            </button>
           </div>
-          <div className='col-4'>
-            <h5>
-              {product.provider}
-            </h5>
+          <div className='col-10'>
+          <TitleProductComponent title={product.name}/>
+          </div>
+          
+        </div>
+        <div className='row mx-5 mb-5'>
+          <h4 className='inter'>{product.provider}</h4>
+        </div>
+        <div className='row m-5 justify-content-center'>
+          <div className='col-10 col-md-10 col-lg-6 col-xl-5 card-detail border rounded'>
+              <div className='box-custom-product mx-4'>
+                  <div className='rounded-circle border border-5 border-white text-center'>
+                    <ImageComponent
+                      hash={product.hash}
+                      claseHash={'rounded-circle border border-gris-12 d-block w-100'}
+                      path={imagePath}
+                      clase={'rounded-circle border border-gris-12 d-block w-100 img-responsive'}
+                      imageAlt={product.name}
+                    />
+                  </div>
+              </div>
+          </div>
+          <div className='col-11 col-md-10 col-lg-8 col-xl-7 p-4'>
+
             {splitDescription(product.description)}
           
           </div>
