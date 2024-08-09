e91477758734275182ee1161942a47a7e4983e03
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-08-09 17:37:55 -0300

    Update AboutPage.jsx

--- src/about/AboutPage.jsx
+++ src/about/AboutPage.jsx
@@ -12,14 +12,14 @@ export const AboutPage = () => {
       {/* <BreakpointIndicator/> */}
         <div className='row justify-content-center m-3'>
           {/* Mision */}
-          <div className='col-10 col-md-8 my-3'>
+          <div className='col-12 col-sm-10 col-md-8 my-3'>
             <AboutComponent
                 title={info[0].title}
                 text={info[0].text}
                 />
           </div>
           {/* Vision */}
-          <div className='col-10 col-md-8 my-3'>
+          <div className='col-12 col-sm-10 col-md-8 my-3'>
           <AboutComponent
                 title={info[1].title}
                 text={info[1].text}
@@ -28,13 +28,13 @@ export const AboutPage = () => {
         </div>
         {/* Valores */}
         <div className="row justify-content-center mt-3">
-          <div className="col-10 col-md-8 ms-5 mt-3">
+          <div className="col-12 col-sm-10 col-md-8 ps-5 ms-md-5 mt-3">
             <h5 className="rozha-one fs-4">
               {info[2].title}
             </h5>
           </div>
         </div>
-        <div className="row justify-content-center mb-3 mx-5 inter">
+        <div className="row justify-content-center mb-3 mx-4 mx-sm-5 inter">
             {info[2].texts.map((val)=>(
               <ValuesComponent key={val.id}
                                 title={val.title}
@@ -44,7 +44,7 @@ export const AboutPage = () => {
         </div>
         {/* Nuestra historia */}
         <div className="row justify-content-center align-items-center m-3">
-          <div className='col-10 col-md-8 my-3'>
+          <div className='col-12 col-sm-10 col-md-8 my-3'>
             <AboutComponent
                   title={info[3].title}
                   text={info[3].text}
@@ -53,25 +53,25 @@ export const AboutPage = () => {
           </div>
         </div>
         {/* Segunda parte de nuestra historia */}
-        <div className="row justify-content-center align-items-center mx-3 custom-component">
-          <div className='col-10 col-md-8 col-lg-5 col-xxl-6 my-3 px-5'>
+        <div className="row justify-content-center align-items-center mx-sm-3 custom-component">
+          <div className='col-12 col-sm-10 col-md-8 col-lg-5 col-xxl-6 my-3 px-5'>
           <AboutComponent
                 text={info[4].text}
                 />
           </div>
-          <div className="col-7 col-md-5 col-lg-3 col-xxl-2 my-3 custom-image">
+          <div className="col-6 col-md-5 col-lg-3 col-xxl-2 my-4 custom-image">
             <img className="img-fluid border rounded" src={'img/nosotrosv.webp'} 
             alt="imagen-oficina-exterior"/>
           </div>
         </div>
 
         {/* Habilitaciones */}
-        <div className="row justify-content-center mt-3 mx-5">
+        <div className="row justify-content-center mt-3 mx-3 mx-sm-5">
           <div className='col-10 col-md-8'>
             <h5 className="rozha-one fs-4">Habilitaciones</h5>
           </div>
         </div>
-        <div className="row justify-content-center mb-3 mx-5">
+        <div className="row justify-content-center mb-3 mx-3 mx-sm-5">
           <div className="col-10 col-md-8 d-md-flex border rounded border-5 border-gris">
             <div className='col-10 col-md-6 my-3 mx-auto'>
               <img className="img-fluid" src={'img/anmat.webp'} alt="logo-anmat"/>
